6b50192281a38def75b2d37c98bb98a53fef074e
GullermoVillarreal <gvillareal@guaodev.com>
2021-11-06 11:15:28 -0600

    components created

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const footer = () => {
+  return (
+    <div>
+      {/* AQUI PUEDEN CREEAR EL FOOTER A SU GUSTO :D */}
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default footer;
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = () => {
+  return (
+    <div>
+      {/* AQUI PUEDEN CREEAR EL HEADER A SU GUSTO :D */}
+      <h1>HEADER</h1>
+    </div>
+  );
+};
+
+export default header;
