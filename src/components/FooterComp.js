88146729b4971c0f22dd62f1e77890166995f2d0
Alicepeach <alicepeachmelocoton2@gmail.com>
2021-11-07 00:40:26 -0600

    newsletter

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col, Button } from 'react-bootstrap';
 
-import { FaBeer } from 'react-icons/fa';
 import { AiFillFacebook } from 'react-icons/ai';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import Form from 'react-bootstrap/Form'
+
 
 export default class FooterComp extends Component {
   render() {
     return (
       <div className="footer-container">
-        <Card.Header>Featured</Card.Header>
+            <Card.Header>
+
+            </Card.Header>
+           
         <Card.Body>
           <Card.Title>Ponte en contacto con nosotros</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-            <div className="alice-flex">
-              <a href="https://www.facebook.com/">
-                <AiFillFacebook fontSize={50} />
-              </a>
-              <FaBeer fontSize={50} />
+                <Card.Text>
+                   <div className="newsletter-flex">
+                   <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                      <span class="input-group-text" id="basic-addon2">@example.com</span>
+                    </div>
+                        
+                    </div>
+
+                <div className="alice-flex">
+                    <a href="https://www.facebook.com/hurakant/">
+                    <AiFillFacebook fontSize={50} />
+                    </a>
+
+                    <a href="https://www.instagram.com/hurikant/">
+                        <AiOutlineInstagram fontSize={50} />
+                    </a>
             </div>
           </Card.Text>
         </Card.Body>
