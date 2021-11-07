dca0bbff616d316692a51a856cf028f4553eacce
GullermoVillarreal <gvillareal@guaodev.com>
2021-11-06 20:29:49 -0600

    iframe

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import heroImg from './img/heroImg.png';
+import Iframe from 'react-iframe';
 const heroSection = () => {
   return (
-    <div>
-      <p className="hero-text">
+    <div className="hero-container">
+      {/* <img src={heroImg} alt="heroImg" className="hero-img" /> */}
+      <Iframe
+        url="https://storage.googleapis.com/tf-tickets/Prueba1.html"
+        width="1200px"
+        height="850px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"
+      />
+      {/*   <p className="hero-text">
         No podemos controlar el clima, pero podemos prepararte
-      </p>
-      <img src={heroImg} alt="heroImg" className="hero-section" />
+      </p> */}
     </div>
   );
 };
