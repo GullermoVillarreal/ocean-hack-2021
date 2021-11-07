904f8a55e199013301be50e494194a2e839c7af6
GullermoVillarreal <gvillareal@guaodev.com>
2021-11-06 23:13:28 -0600

    icons

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
 
+import { FaBeer } from 'react-icons/fa';
+import { AiFillFacebook } from 'react-icons/ai';
+
 export default class FooterComp extends Component {
   render() {
     return (
       <div className="footer-container">
-        <Card>
-          <Card.Header>Featured</Card.Header>
-          <Card.Body>
-            <Card.Title>Ponte en contacto con nosotros</Card.Title>
-            <Card.Text>
-              With supporting text below as a natural lead-in to additional
-              content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer className="text-muted">By Hurakan't</Card.Footer>
-        </Card>
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Card.Title>Ponte en contacto con nosotros</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+            <div className="alice-flex">
+              <a href="https://www.facebook.com/">
+                <AiFillFacebook fontSize={50} />
+              </a>
+              <FaBeer fontSize={50} />
+            </div>
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted">By Hurakan't</Card.Footer>
       </div>
     );
   }
